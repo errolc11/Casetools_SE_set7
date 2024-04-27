56c45880969f9a46f09eb1aca267f97e641e48a5
Errol Caeiro <errolcaeiro07@gmail.com>
2024-04-27 12:30:58 +0530

    commit code

---
+++ Casetools_SE_set7/src/exam/Rectangle.java
@@ -0,0 +1,27 @@
+package exam;
+
+public class Rectangle {
+	private double width;
+	private double height;
+	public Rectangle(double width,double height) {
+		this.width=width;
+		this.height=height;
+	}
+	public double getWidth() {
+		return width;
+	}
+	public void setWidth(double width) {
+		this.width=width;
+	}
+	public double getHeight() {
+		return height;
+	}
+	public void setHeight(double height) {
+		this.height=height;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
